35285393d3d9deed6d868a78dbb45cac515351a7
Nicholas Kovacs <110848072+N-Kovacs@users.noreply.github.com>
2023-06-09 20:52:29 -0400

    more skeleton

--- src/components/home/cards/individualothercard.jsx
+++ src/components/home/cards/individualothercard.jsx
@@ -2,3 +2,28 @@
 //pass in status
 //highlight grey if used
 //in later rounds turn green if higher and red if lower
+
+import { useState, useEffect } from "react";
+
+import reactLogo from "../../assets/react.svg";
+
+import "./Home.css";
+
+export default function Home(props) {
+  const [btnState, setBtnState] = useState("");
+  const [hoverImageState, setHoverImageState] = useState("");
+
+  const clicked = () => {
+    props.transition(btnState);
+  };
+
+  const divStyle = {
+    backgroundImage: hoverImageState,
+  };
+
+  return (
+    <div>
+        
+    </div>
+  );
+}
