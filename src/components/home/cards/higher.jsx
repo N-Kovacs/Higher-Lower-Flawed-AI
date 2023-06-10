96351fb6f35ef871d0e24806c5be0cc89994c830
Nicholas Kovacs <110848072+N-Kovacs@users.noreply.github.com>
2023-06-10 16:19:10 -0400

    reactskeleton

--- src/components/home/cards/higher.jsx
+++ src/components/home/cards/higher.jsx
@@ -1,1 +1,25 @@
 //higher
+import { useState, useEffect } from "react";
+
+import reactLogo from "../../assets/react.svg";
+
+import "./Home.css";
+
+export default function Home(props) {
+  const [btnState, setBtnState] = useState("");
+  const [hoverImageState, setHoverImageState] = useState("");
+
+  const clicked = () => {
+    props.transition(btnState);
+  };
+
+  const divStyle = {
+    backgroundImage: hoverImageState,
+  };
+
+  return (
+    <div>
+        
+    </div>
+  );
+}
