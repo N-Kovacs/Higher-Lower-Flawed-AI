26cb0b740c3f6c173094c58353da45ecaa37bf4b
N.Kovacs <nicholasderekkovacs@gmail.com>
2023-06-21 14:32:31 -0400

    card css changes

--- src/stories/higher.stories.js
+++ src/stories/higher.stories.js
@@ -12,26 +12,6 @@ export default {
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    label: "Button",
   },
 };
