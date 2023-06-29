88fb85550ced16765ebc36b6a663a791a001c0b9
Nicholas Kovacs <110848072+N-Kovacs@users.noreply.github.com>
2023-06-28 22:50:22 -0400

    higher stories

--- src/stories/higher.stories.js
+++ src/stories/higher.stories.js
@@ -9,29 +9,29 @@ export default {
   },
 };
 
-export const Primary = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
+// export const Primary = {
+//   args: {
+//     primary: true,
+//     label: 'Button',
+//   },
+// };
 
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
+// export const Secondary = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
 
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
+// export const Large = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
 
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+// export const Small = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };
