6a3f8cb4d68756b72042a1d3fdf73ffcd49917c0
Nicholas Kovacs <110848072+N-Kovacs@users.noreply.github.com>
2023-06-29 20:28:23 -0400

    menu css

--- src/components/home/smart.jsx
+++ src/components/home/smart.jsx
@@ -1,1 +1,36 @@
 //card
+import { useState, useEffect } from "react";
+import CanvasJSReact from "@canvasjs/react-charts";
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+import "./Home.css";
+
+export default function Home(props) {
+  const options = {
+    title: {
+      text: "Basic Column Chart in React",
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: [
+          { label: "Apple", y: 10 },
+          { label: "Orange", y: 15 },
+          { label: "Banana", y: 25 },
+          { label: "Mango", y: 30 },
+          { label: "Grape", y: 28 },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <CanvasJSChart
+        options={options}
+        /* onRef = {ref => this.chart = ref} */
+      />
+    </div>
+  );
+}
